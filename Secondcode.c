e727161d94d511a861d4c0e4ad9af5fbea2f28b2
Amul Kamlapure <akamalapure@gmail.com>
2025-11-21 00:03:32 +0530

    third program modified for debug practice

--- Secondcode.c
+++ Secondcode.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void main() {
     printf("Greetings from SecondCode.c!\n");
 }
--- ThirdCode.cpp
+++ ThirdCode.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 int main() {
-    std::cout << "Hello from ThirdCode.cpp!" << std::endl;
+    int sum = 0;
+    int A[] = {1, 2, 3, 4, 5};
+    for(int  num : A) {
+        sum += num;
+        std::cout << "Current Sum: " << sum << std::endl;
+    }
+    std::cout << "Final Sum: " << sum << std::endl;
+    
     return 0;
 }
