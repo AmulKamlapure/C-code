99db70feeae434b13549dfa22690b2c5b81a9b4a
Amul Kamlapure <akamalapure@gmail.com>
2025-11-21 01:38:26 +0530

    added some basic string ops

--- MathOps.cpp
+++ MathOps.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
+
 int main(){
+    std::string name;
     std::cout <<"Enter two numbers: ";
     int a, b, c;
     std::cin >> a >> b;
-    c = a + b;
-    std::cout << "Addition: " << c;
+    std::cin.ignore(); // To ignore the newline character left in the buffer
+    std::cout << "Enter name ";
+    
+    std::getline(std::cin, name);
+    std::cout << "Hello " << name << std::endl;
+   // c = a + b;
+    std::cout << "Addition: " << a;
     return 0;
 }
